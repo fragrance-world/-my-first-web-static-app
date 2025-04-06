60b48461c3ca8bf9c43e448bcdfca185083a0070
fragrance-world <social@ghuroobnet.com>
2025-04-06 23:17:37 +0400

    Updates

---
+++ precache-manifest.29c2305a1b4ef74d020a3c7905efb5df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc61072deeeb67f7f0e293696c944110",
+    "url": "/my-first-web-static-app/index.html"
+  },
+  {
+    "revision": "2af665461250b127299b",
+    "url": "/my-first-web-static-app/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "874eea42154ea1f56df5",
+    "url": "/my-first-web-static-app/static/js/2.d84c9f37.chunk.js"
+  },
+  {
+    "revision": "2af665461250b127299b",
+    "url": "/my-first-web-static-app/static/js/main.10b26e11.chunk.js"
+  },
+  {
+    "revision": "a6311ea6f14631342718",
+    "url": "/my-first-web-static-app/static/js/runtime~main.6aa9cc38.js"
+  }
+]);
---
+++ static/js/main.10b26e11.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(n,e,t){n.exports=t(9)},8:function(n,e,t){},9:function(n,e,t){"use strict";t.r(e);var o=t(0),r=t.n(o),c=t(2),l=t.n(c);t(8);var u=function(){return r.a.createElement("div",null,"Hello ","World")};l.a.render(r.a.createElement(u,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.10b26e11.chunk.js.map
